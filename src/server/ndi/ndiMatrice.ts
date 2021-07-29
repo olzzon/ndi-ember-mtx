7a287a0ee21101af3986331b78adb8b7fd87a922
olzzon <github@olzzon.dk>
2021-07-29 13:20:11 +0200

    feat: better NDI lib error handling

--- src/server/ndi/ndiMatrice.ts
+++ src/server/ndi/ndiMatrice.ts
@@ -1,9 +1,21 @@
+import { logger } from '../utils/logger'
+
 const ndi_mtx = require('bindings')('ndi_mtx')
 
-export const initializeNdiRouting = (url: string, targetLabel: string, targetIndex: number) => {
-    ndi_mtx.initializeRouting(url, targetLabel, targetIndex)
+export const initializeNdiRouting = (
+    url: string,
+    targetLabel: string,
+    targetIndex: number
+) => {
+    let status = ndi_mtx.initializeRouting(url, targetLabel, targetIndex)
+    if ('completed' !== status) {
+        logger.error(`NDI Error : ${status}`)
+    }
 }
 
 export const changeNdiRoutingSource = (url: string, targetIndex) => {
-    ndi_mtx.changeRoutingSource(url, targetIndex)
+    let status = ndi_mtx.changeRoutingSource(url, targetIndex)
+    if ('completed' !== status) {
+        logger.error(`NDI Error : ${status}`)
+    }
 }
--- src_c/ndi_mtx.cc
+++ src_c/ndi_mtx.cc
@@ -7,151 +7,151 @@ NDIlib_routing_instance_t pNDI_routing[100];
 
 napi_value changeRoutingSource(napi_env env, napi_callback_info info)
 {
-  napi_valuetype type;
-  napi_status status;
-  napi_value result;
+    napi_valuetype type;
+    napi_status status;
+    napi_value result;
 
-  // Read arguments passed to promise
-  size_t argc = 2;
-  napi_value args[2];
-  status = napi_get_cb_info(env, info, &argc, args, nullptr, nullptr);
+    // Read arguments passed to promise
+    size_t argc = 2;
+    napi_value args[2];
+    status = napi_get_cb_info(env, info, &argc, args, nullptr, nullptr);
 
-  if (argc != (size_t)2)
-  {
-    printf("Wrong args NDI\n");
-    return NULL;
-  }
+    if (argc != (size_t)2)
+    {
+        status = napi_create_string_utf8(env, "Error : wrong args", NAPI_AUTO_LENGTH, &result);
+        return result;
+    }
 
-  const char *source = NULL;
-  size_t sourcel;
+    const char *source = NULL;
+    size_t sourcel;
 
-  status = napi_typeof(env, args[0], &type);
-  if (type != napi_string)
-  {
-    printf("Wrong type - NDI source \n");
-    return NULL;
-  }
+    status = napi_typeof(env, args[0], &type);
+    if (type != napi_string)
+    {
+        status = napi_create_string_utf8(env, "Error : Wrong type - NDI Source", NAPI_AUTO_LENGTH, &result);
+        return result;
+    }
 
-  status = napi_get_value_string_utf8(env, args[0], NULL, 0, &sourcel);
-  source = (char *)malloc(sourcel + 1);
-  status = napi_get_value_string_utf8(env, args[0], (char *)source, sourcel + 1, &sourcel);
+    status = napi_get_value_string_utf8(env, args[0], NULL, 0, &sourcel);
+    source = (char *)malloc(sourcel + 1);
+    status = napi_get_value_string_utf8(env, args[0], (char *)source, sourcel + 1, &sourcel);
 
-  printf("NDI Source Url : %s \n", source);
+    printf("NDI Source Url : %s \n", source);
 
-  // Populate carrier with new instance
-  NDIlib_source_t *ndi_source = new NDIlib_source_t();
-  ndi_source->p_ndi_name = "";
-  ndi_source->p_url_address = source;
+    // Populate carrier with new instance
+    NDIlib_source_t *ndi_source = new NDIlib_source_t();
+    ndi_source->p_ndi_name = "";
+    ndi_source->p_url_address = source;
 
-  int target_index;
-  status = napi_get_value_int32(env, args[1], &target_index);
+    int target_index;
+    status = napi_get_value_int32(env, args[1], &target_index);
 
-  NDIlib_routing_change(pNDI_routing[target_index], ndi_source);
+    NDIlib_routing_change(pNDI_routing[target_index], ndi_source);
 
-  printf("NDI Changing Target Index %i to Source %s \n", target_index, source);
-  napi_value str;
-  status = napi_create_string_utf8(env, "hello world", NAPI_AUTO_LENGTH, &str);
-  assert(status == napi_ok);
+    printf("NDI Changing Target Index %i to Source %s \n", target_index, source);
+    status = napi_create_string_utf8(env, "completed", NAPI_AUTO_LENGTH, &result);
 
-  return str;
+    assert(status == napi_ok);
+    return result;
 }
 
 napi_value initializeRouting(napi_env env, napi_callback_info info)
 {
-  napi_valuetype type;
-  napi_status status;
+    napi_valuetype type;
+    napi_status status;
+    napi_value result;
 
-  // Read arguments passed to promise
-  size_t argc = 3;
-  napi_value args[3];
-  status = napi_get_cb_info(env, info, &argc, args, nullptr, nullptr);
+    // Read arguments passed to promise
+    size_t argc = 3;
+    napi_value args[3];
+    status = napi_get_cb_info(env, info, &argc, args, nullptr, nullptr);
 
-  if (argc != (size_t)3)
-  {
-    printf("Wrong args\n");
-    return NULL;
-  }
+    if (argc != (size_t)3)
+    {
+        status = napi_create_string_utf8(env, "Error : wrong args", NAPI_AUTO_LENGTH, &result);
+        return result;
+    }
 
-  const char *source = NULL;
-  size_t sourcel;
+    const char *source = NULL;
+    size_t sourcel;
 
-  status = napi_typeof(env, args[0], &type);
-  if (type != napi_string)
-  {
-    printf("Wrong type - NDI source \n");
-    return NULL;
-  }
+    status = napi_typeof(env, args[0], &type);
+    if (type != napi_string)
+    {
+        status = napi_create_string_utf8(env, "Error : Wrong type - NDI Source", NAPI_AUTO_LENGTH, &result);
+        return result;
+    }
 
-  status = napi_get_value_string_utf8(env, args[0], NULL, 0, &sourcel);
-  source = (char *)malloc(sourcel + 1);
-  status = napi_get_value_string_utf8(env, args[0], (char *)source, sourcel + 1, &sourcel);
+    status = napi_get_value_string_utf8(env, args[0], NULL, 0, &sourcel);
+    source = (char *)malloc(sourcel + 1);
+    status = napi_get_value_string_utf8(env, args[0], (char *)source, sourcel + 1, &sourcel);
 
-  printf("NDI Source Url : %s \n", source);
+    printf("NDI Source Url : %s \n", source);
 
-  const char *target = NULL;
-  size_t targetl;
+    const char *target = NULL;
+    size_t targetl;
 
-  status = napi_typeof(env, args[1], &type);
-  if (type != napi_string)
-  {
-    printf("Wrong type - NDI Target \n");
-    return NULL;
-  }
+    status = napi_typeof(env, args[1], &type);
+    if (type != napi_string)
+    {
+        printf("Wrong type - NDI Target \n");
+        return NULL;
+    }
 
-  status = napi_get_value_string_utf8(env, args[1], NULL, 0, &targetl);
-  target = (char *)malloc(targetl + 1);
-  status = napi_get_value_string_utf8(env, args[1], (char *)target, targetl + 1, &targetl);
+    status = napi_get_value_string_utf8(env, args[1], NULL, 0, &targetl);
+    target = (char *)malloc(targetl + 1);
+    status = napi_get_value_string_utf8(env, args[1], (char *)target, targetl + 1, &targetl);
 
-  printf("NDI Target Name : %s \n", target);
+    printf("NDI Target Name : %s \n", target);
 
-  int target_index;
-  status = napi_get_value_int32(env, args[2], &target_index);
+    int target_index;
+    status = napi_get_value_int32(env, args[2], &target_index);
 
-  // Populate carrier with new instance
-  NDIlib_source_t *ndi_source = new NDIlib_source_t();
-  ndi_source->p_ndi_name = "";
-  ndi_source->p_url_address = source;
+    // Populate carrier with new instance
+    NDIlib_source_t *ndi_source = new NDIlib_source_t();
+    ndi_source->p_ndi_name = "";
+    ndi_source->p_url_address = source;
 
-  // Create NDI target
-  NDIlib_routing_create_t NDI_send_create_desc;
-  NDI_send_create_desc.p_ndi_name = target;
+    // Create NDI target
+    NDIlib_routing_create_t NDI_send_create_desc;
+    NDI_send_create_desc.p_ndi_name = target;
 
-  // We create the NDI routing
-  pNDI_routing[target_index] = NDIlib_routing_create(&NDI_send_create_desc);
-  if (!pNDI_routing[target_index])
-    return NULL;
+    // We create the NDI routing
+    pNDI_routing[target_index] = NDIlib_routing_create(&NDI_send_create_desc);
+    if (!pNDI_routing[target_index])
+        return NULL;
 
-  NDIlib_routing_change(pNDI_routing[target_index], ndi_source);
+    NDIlib_routing_change(pNDI_routing[target_index], ndi_source);
 
-  printf("NDI Routing initialized!!!! \n");
-  napi_value str;
-  status = napi_create_string_utf8(env, "INITIALIZED", NAPI_AUTO_LENGTH, &str);
-  assert(status == napi_ok);
+    printf("NDI Routing initialized!!!! \n");
+    napi_value str;
+    status = napi_create_string_utf8(env, "completed", NAPI_AUTO_LENGTH, &result);
 
-  return str;
+    assert(status == napi_ok);
+    return result;
 }
 
 static napi_value Init(napi_env env, napi_value exports)
 {
-  napi_value initialize_fn;
-  napi_status status;
-  status = napi_create_function(
-      env, "", NAPI_AUTO_LENGTH, initializeRouting, NULL, &initialize_fn);
+    napi_value initialize_fn;
+    napi_status status;
+    status = napi_create_function(
+        env, "", NAPI_AUTO_LENGTH, initializeRouting, NULL, &initialize_fn);
 
-  status = napi_set_named_property(env, exports, "initializeRouting", initialize_fn);
-  if (status != napi_ok)
-    return NULL;
+    status = napi_set_named_property(env, exports, "initializeRouting", initialize_fn);
+    if (status != napi_ok)
+        return NULL;
 
-  napi_value change_source_fn;
-  status = napi_create_function(
-      env, "", NAPI_AUTO_LENGTH, changeRoutingSource, NULL, &change_source_fn);
+    napi_value change_source_fn;
+    status = napi_create_function(
+        env, "", NAPI_AUTO_LENGTH, changeRoutingSource, NULL, &change_source_fn);
 
-  status = napi_set_named_property(env, exports, "changeRoutingSource", change_source_fn);
-  if (status != napi_ok)
-    return NULL;
+    status = napi_set_named_property(env, exports, "changeRoutingSource", change_source_fn);
+    if (status != napi_ok)
+        return NULL;
 
-  assert(status == napi_ok);
-  return exports;
+    assert(status == napi_ok);
+    return exports;
 }
 
 NAPI_MODULE(NODE_GYP_MODULE_NAME, Init)
