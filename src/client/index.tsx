7d041f237adeb1daeae43b7a2f0f991b152a3936
olzzon <github@olzzon.dk>
2021-08-25 12:31:41 +0200

    feat: client panel for controlling 1 target (http://localhost:3008/?target=xx)

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -2,11 +2,24 @@ import ReactDOM from 'react-dom'
 import React from 'react'
 import MainPage from './components/mainPage'
 import Matrix from './components/matrix'
+import ClientPanel from './components/clientpanel'
+
+const target = new URLSearchParams(window.location.search).get('target')
+let targetIndex: number = -1
+if (target) {
+    targetIndex = parseInt(target) - 1
+}
 
 ReactDOM.render(
     <React.StrictMode>
-        <MainPage />
-        <Matrix />
+        {targetIndex === -1 ? (
+            <React.Fragment>
+                <MainPage />
+                <Matrix />
+            </React.Fragment>
+        ) : (
+            <ClientPanel targetIndex={targetIndex} />
+        )}
     </React.StrictMode>,
     document.getElementById('root')
 )
